f2cda83b495164f139d271be52faf517d6582192
Guydstack <129586924+Guydstack@users.noreply.github.com>
2023-06-01 23:54:55 +0300

    initial commit

--- script.js
+++ script.js
@@ -12,9 +12,12 @@ function checkWhoIsBigger(arr){
 function tester(){
     document.getElementById("ex_1a").innerHTML = checkWhoIsBigger(postivNumbers)
     document.getElementById("ex_1b").innerHTML = checkWhoIsBigger(negativNumbers)
-    document.getElementById("input_array_result").innerHTML = checkIfInputIsAnArray(myArray);
+    document.getElementById("input_array_result").innerHTML = "Is '[1, 2, 4, 0]' an array ? " + checkIfInputIsAnArray(myArray);
+    document.getElementById("input_string_result").innerHTML = "Is 'w3resource' an array ? " + checkIfInputIsAnArray(myText);
+    document.getElementById("trn_tostring_result").innerHTML = arrayToString(myColor);
+    document.getElementById("sort_items_result").innerHTML = sortItemsInArray(arr1);
+    document.getElementById("remove_items_result").innerHTML = removeElementFromArray(arr2,5);
 }
-
 
 // Math: Exercise 2
 // Write a JavaScript function to find the lowest value in an array.
@@ -134,13 +137,103 @@ function getNumberOfDaysInAMonth(year, month){
     document.getElementById("ex_date_2d").innerHTML = daysInDecember;
   }
 
+// Datetime: Exercise 3
+//   Write a JavaScript program to calculate age.
+const getAge = birthDate => Math.floor((new Date() - new Date(birthDate).getTime()) / 3.15576e+10)
+
+console.log(getAge('1982-11-04'))
+console.log(getAge('1962-01-01'))
+
+
+// Datetime: Exercise 4
+// Write a JavaScript function to get time differences in hours between two dates.
+
+function differencesInHours(){
+ let date1 = new Date(2018, 11, 24, 12, 33, 30, 0).getHours()
+ let date2 = new Date(2018, 11, 24, 10, 33, 30, 0).getHours()
+
+ return date1-date2
+}
+
+console.log(differencesInHours());
+
+// Test Data :
+// dt1 = new Date("October 13, 2014 08:11:00");
+// dt2 = new Date("October 13, 2014 11:13:00");
+// console.log(diff_hours(dt1, dt2));
+
+
 
 // Array: Exercise 1 
 // Write a JavaScript function to check whether an `input` is an array or not.
+
 const myArray = [1, 2, 4, 0];
 let myText = 'w3resource';
-// let arrayResult =  Array.isArray();
 
 function checkIfInputIsAnArray(check){
-    return Array.isArray(...check)
+    return Array.isArray(check)
 }
+
+
+// Array: Exercise 2 
+// Write a simple JavaScript program to join all elements of the following array into a string.
+
+const myColor = ["Red", "Green", "White", "Black"];
+
+function arrayToString(transferToString){
+    return transferToString.toString()
+}
+
+
+// Array: Exercise 3 
+// Write a JavaScript program to sort the items of an array.
+
+const arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+
+function sortItemsInArray(sortAnArray){
+   return sortAnArray.sort(function(a, b){return a-b}).toString();
+}
+
+// Array: Exercise 4
+// Write a JavaScript function to remove a specific element from an array.
+
+const arr2 = [2, 5, 9, 6];
+
+function removeElementFromArray(removeEl,n){
+
+   const index = removeEl.indexOf(n);
+   if (index > -1) {
+    removeEl.splice(index, 1);
+}
+   return removeEl;
+}
+
+
+// Array: Exercise 5
+// Write a JavaScript function to find an array containing a specific element.
+
+function checkElementFromArray(removeEl,n){
+    for (var i = 0; i < removeEl.length; i++) {
+        if (removeEl[i] === n) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(checkElementFromArray(arr2,5));
+
+
+// Array: Exercise 6
+// Write a JavaScript function to get random items from an array.
+
+function getRendomItemsFromArray(arrayItems){
+
+        const randomIndex = Math.floor(Math.random() * arrayItems.length);
+        const item = arrayItems[randomIndex];
+        return item;
+    }
+        
+    console.log(getRendomItemsFromArray(arr2));
+
+
